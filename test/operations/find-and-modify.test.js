93c290096a787052e4ae13f2728b4d56e573a6ab
Javier Lecuona <javierlecuona@gmail.com>
2017-09-20 13:14:27 -0300

    Added find and modify test

--- test/operations/find-and-modify.test.js
+++ test/operations/find-and-modify.test.js
@@ -32,4 +32,34 @@ describe('findAndModify tests', () => {
       })
       .catch(done);
   });
+
+  it('FindAndModify callback test', (done) => {
+    const db = new Db('find-and-modify-test');
+    const collection = db.collection('test');
+    collection.insertMany([{ a: 1 }, { b: 1 }, { c: 1 }], { w: 1 }, (err0, insertResult) => {
+      expect(err0).to.not.exist;
+      expect(insertResult).to.exist;
+      collection.findAndModify({ a: 1 }, [['a', 1]], { $set: { b1: 1 } }, { new: true }, (err, doc) => {
+        expect(err).to.not.exist;
+        expect(doc).to.exist;
+        expect(1).to.eql(doc.value.a);
+        expect(1).to.eql(doc.value.b1);
+        collection.findAndModify({ b: 1 }, [['b', 1]], { $set: { b: 2 } }, { remove: true }, (err1, result) => {
+          expect(err1).to.not.exist;
+          expect(result).to.exist;
+          collection.findOne({ b: 1 }, (err2, item) => {
+            expect(err2).to.not.exist;
+            expect(item).to.not.exist;
+            collection.findAndModify({ d: 1 }, [['b', 1]],
+              { d: 1, f: 1 }, { new: true, upsert: true, w: 1 }, (err3, doc1) => {
+                expect(err3).to.not.exist;
+                expect(1).to.eql(doc1.value.d);
+                expect(1).to.eql(doc1.value.f);
+                done();
+              });
+          });
+        });
+      });
+    });
+  });
 });
