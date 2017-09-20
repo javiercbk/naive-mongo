85c401750b14ab1a794c7cf01003d1dc6da81684
Javier Lecuona <javierlecuona@gmail.com>
2017-09-20 00:12:59 -0300

    Fixed cursor

---
+++ test/operations/find.test.js
@@ -0,0 +1,37 @@
+/* global describe, it */
+const chai = require('chai');
+require('mocha');
+
+const Db = require('../../lib/db');
+
+const { expect } = chai;
+
+describe('Find tests', () => {
+  it('Find test', (done) => {
+    const db = new Db('find-test');
+    const collection = db.collection('test');
+    collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }], { w: 1 })
+      .then(() => collection.find().toArray())
+      .then((docs) => {
+        expect(3).to.eql(docs.length);
+        done();
+      })
+      .catch(done);
+  });
+});
+
+it('Find with skip limit', (done) => {
+  const db = new Db('find-test');
+  const collection = db.collection('test');
+  collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }], { w: 1 })
+    .then(() => collection.find().skip(1).limit(1).project({ b: 1 })
+      .toArray())
+    .then((docs) => {
+      expect(1).to.eql(docs.length);
+      // eslint-disable-next-line no-unused-expressions
+      expect(docs[0].b).to.be.undefined;
+      expect(2).to.eql(docs[0].a);
+      done();
+    })
+    .catch(done);
+});
